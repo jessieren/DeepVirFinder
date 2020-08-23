7fce8d57c188ce0c171829cc04bef4bab860c414
nikos <n.pappas@uu.nl>
2020-08-23 20:21:58 +0200

    setup and manifest for required data

---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+## Following example from
+## https://github.com/pypa/sampleproject/blob/master/setup.py
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deepvirfinder",
+    version="1.0.0",
+    author="Jie Ren",
+    author_email="renj@usc.edu",
+    description="Identifying viruses from metagenomic data by deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # Using my fork for now
+    url="https://github.com/papanikos/DeepVirFinder",
+    keywords="machine learning, bioinformatics, metagenomics, viromics",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+                ],
+    include_package_data=True,
+    python_requires='>=3.6',
+
+    # The fine grained versions are the ones
+    # used during development, installed with
+    # $ conda create -n dvf python=3.6 biopython numpy theano keras scikit-learn
+    install_requires=[
+                    'biopython>=1.77',
+                    'keras>=2.3.1',
+                    'numpy>=1.17.0',
+                    'scikit-learn>=0.23.1',
+                    'theano>=1.0.4',
+                    ],
+    scripts=[
+        'deepvirfinder/dvf.py',
+        'deepvirfinder/encode.py',
+        'deepvirfinder/training.py',
+        ],
+
+    # Could not make it work like this
+    # TO DO
+#    entry_points={
+#        'console_scripts' : [
+#           'dvf=dvf.py',
+#            ],
+#        }
+    )
+
